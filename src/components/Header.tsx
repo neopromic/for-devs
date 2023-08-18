81d55418df011438443333592da1d38f156f2ee3
neopromic <cusaowesley1@gmail.com>
2023-08-18 15:06:53 -0300

    feat: (delete) removed NavBar item unecessary

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,9 +15,6 @@ export default function Header() {
       </NavbarBrand>
 
       <NavbarContent justify="end">
-        <NavbarItem className=" lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
