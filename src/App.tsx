4aec25dbafb108ea702afa90b876d42d61973e79
neopromic <cusaowesley1@gmail.com>
2023-08-19 15:18:44 -0300

    feat: estrutura refeita

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Header from "./components/Header";
-import SectionWelcome from "./components/SectionWelcome";
-import "./styles/App.css";
 import { NextUIProvider } from "@nextui-org/react";
+import "./styles/App.css"
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <NextUIProvider>
       <Header />
-      <SectionWelcome />
+      <Main />
     </NextUIProvider>
   );
 }
