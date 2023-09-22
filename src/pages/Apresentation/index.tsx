b4daeb5e1e56d8f7838c048029d81b7a6c050ff6
neopromic <cusaowesley1@gmail.com>
2023-09-22 11:01:22 -0300

    fix: removed unused components

--- src/pages/Apresentation/index.tsx
+++ src/pages/Apresentation/index.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import { Button } from "@/components/ui/button";
+
 export function Apresentation() {
   return (
   <>
