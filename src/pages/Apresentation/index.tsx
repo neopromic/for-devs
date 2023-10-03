3d6f59a852f75654a0ec984765f91cbe978bad23
neopromic <cusaowesley1@gmail.com>
2023-10-03 11:49:54 -0300

    fix: removed unused things

--- src/pages/Apresentation/index.tsx
+++ src/pages/Apresentation/index.tsx
@@ -2,12 +2,11 @@ import Header from "@/components/ui/Header";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Github, LucideSparkles } from "lucide-react";
-import { useContext } from "react";
-import { Link } from "react-router-dom"
-import { AuthContext }from "@/utils/api/auth/authContextApi/index"
+// import { useContext } from "react";
+import { Link } from "react-router-dom";
+// import { AuthContext } from "@/utils/api/auth/authContextApi/index";
 
 export function Apresentation() {
-  const { user } = useContext(AuthContext)
   return (
     <>
       <Header />
@@ -20,32 +19,37 @@ export function Apresentation() {
           desenvolvedores.
         </p>
         <div className="flex gap-3">
-
-        <Link className="block" to="/login">
-        <Button className="gap-1">
-          <LucideSparkles size={16} /> Get Started
-          </Button>
-        </Link>
-        <a 
-        href="https://github.com/neopromic/for-devs" 
-        target="_blank">
-          <Button variant={"outline"} size={"icon"}>
-            <Github />
-          </Button>
-        </a>
+          <Link className="block" to="/login">
+            <Button className="gap-1">
+              <LucideSparkles size={16} /> Get Started
+            </Button>
+          </Link>
+          <a href="https://github.com/neopromic/for-devs" target="_blank">
+            <Button variant={"outline"} size={"icon"}>
+              <Github />
+            </Button>
+          </a>
         </div>
         <div className="w-full h-80 border rounded my-4 flex">
           <div className="rounded bg-violet-500/5 h-full w-full p-6 flex flex-col items-center justify-center">
-            <h2 className="font-bold text-xl lg:text-3xl tracking-tighter leading-tight">Colabore!</h2>
-            <p className="text-muted-foreground text-center leading-relaxed ">O ForDevs visa criar uma comunidade acolhedora para desenvolvedores, promovendo colaboração, aprendizado e inovação.</p>
+            <h2 className="font-bold text-xl lg:text-3xl tracking-tighter leading-tight">
+              Colabore!
+            </h2>
+            <p className="text-muted-foreground text-center leading-relaxed ">
+              O ForDevs visa criar uma comunidade acolhedora para
+              desenvolvedores, promovendo colaboração, aprendizado e inovação.
+            </p>
           </div>
           <Separator orientation="vertical" />
           <div className="rounded bg-violet-500/5 h-full w-full p-6 flex flex-col items-center justify-center">
-            <h2 className="font-bold text-xl lg:text-3xl tracking-tighter leading-tight">Heading</h2>
-            <p className="text-muted-foreground text-center leading-relaxed ">Paragraph</p>
+            <h2 className="font-bold text-xl lg:text-3xl tracking-tighter leading-tight">
+              Heading
+            </h2>
+            <p className="text-muted-foreground text-center leading-relaxed ">
+              Paragraph
+            </p>
           </div>
         </div>
-        
       </div>
     </>
   );
